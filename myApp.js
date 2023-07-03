8e1d48d1b9b7d05ea2d3e7822074ddadbbae2b65
khanhcoluy <49581756+khanhcoluy@users.noreply.github.com>
2023-07-03 14:38:03 +0000

    Update data and delete data
    - Update data: findOneAndUpdate(),
    - delete data: findByIdAndRemove() and findOneAndRemove()

--- myApp.js
+++ myApp.js
@@ -86,12 +86,17 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  })
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, res) => {
+    if (err) return done(err);
+    done(null, res);
+  })
 };
 
 const removeManyPeople = (done) => {
